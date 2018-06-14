6181044d82bb81546191f710dd74d5ee02fff898
VegaIsaias <32isaias@gmail.com>
2018-06-13 23:30:41 -0400

    hashing
    Added hasing algorithm, and action cases

--- Hashing.java
+++ Hashing.java
@@ -82,6 +82,103 @@ public class Hashing {
             e.printStackTrace();
         }
     }
+    
+    // Executes input instructions
+    public static void executeInput(ArrayList<Node> input, ArrayList<List> hashTable) {
+        try {
+            String command;
+            hash hashing = null;
+            int key;
+            
+            // Read all input and execute per command
+            for (int x = 0; x < input.size(); x++) {
+                
+                command = input.get(x).getCommand();
+                
+                // Command cases
+                switch(command) {
+                    case "i":   // Insert to hash table
+                        hashing = new hash(input.get(x).name, hashTable.size());
+                        key = hashing.genHash(input.get(x).name, hashTable.size()); 
+                        
+                        
+                        hashTable.get(key).insertAtBack(input.get(x).name); // unchecked type
+                        break;
+                    case "s":   // Search for name in hash table
+                        hashing = new hash(input.get(x).name, hashTable.size());
+                        key = hashing.genHash(input.get(x).name, hashTable.size());
+                        hashTable.get(key).search(input.get(x).name, key);
+                        break;
+                    case "d":   // Delete name from hash table
+                        hashing = new hash(input.get(x).name, hashTable.size());
+                        key = hashing.genHash(input.get(x).name, hashTable.size());
+                        if (hashTable.get(key).isEmpty()) {
+                            System.out.println(input.get(key).name + " not found - not deleted.");
+                        } else {
+                            hashTable.get(key).deleteName(input.get(x).name, key);
+                        }
+                        break;
+                    case "p":   // Print list in hash table
+                        System.out.println("The Hash Table contains:");
+                        for (int y = 0; y < hashTable.size(); y++) {
+                            hashTable.get(y).print(y);
+                        }
+                        break;
+                    case "q":    // EXIT PROGRAM
+                        return;
+                }
+                    
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    // Data Node // Used for storing the command input
+    static class Node {
+        
+        private String command;
+        private String name;
+        private Node next;
+
+        public Node(String comm, String name) {
+            this.command = comm;
+            this.name = name;
+            this.next = null;
+        }
+        
+        public Node(String command) {
+            this.command = command;
+            this.next = null;
+        }
+        
+        public String getCommand() { return command; }
+        public String getName() { return name; }
+        public Node getNext() { return next; }
+        public void setNext(Node next) { this.next = next; }
+    }
+    
+    // Generate hashKey
+    static class hash {
+        String name;
+        int hashKey;
+        int size;
+        
+        public hash(String name, int size) {
+            this.name = name;
+            this.size = size;
+        }
+        
+        // Horners Rule Algo 
+        public static int genHash(String name, int size) {
+            int hash = 0;
+            char[] broken = name.toCharArray();
+            for (int x = 0; x < name.length(); x++) {
+                hash = ((hash * 27) + ((int)broken[x] - 96)) % size;
+            }
+            return hash;
+        }
+    }
 
     
 }
